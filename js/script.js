dbce68300b23cbcc3d66ce9dd48f285758ca1577
Людмила <mila.demidova.02@list.ru>
2025-08-11 17:45:30 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Добавляем плавную прокрутку по якорным ссылкам
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
